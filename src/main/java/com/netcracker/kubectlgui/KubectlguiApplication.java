4f7665d713306a95737bb220d01198177de74b9b
Dmitrii Sobolev <dmitrii.sobolev@netcracker.com>
2022-05-27 22:38:03 +0400

    Initial commit

---
+++ src/main/java/com/netcracker/kubectlgui/KubectlguiApplication.java
@@ -0,0 +1,13 @@
+package com.netcracker.kubectlgui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubectlguiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubectlguiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netcracker/kubectlgui/KubectlguiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netcracker.kubectlgui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubectlguiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
